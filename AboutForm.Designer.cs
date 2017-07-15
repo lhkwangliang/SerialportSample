63edae785ffc5e2cadb5ead81758877ba52c5afb
Walker <lhkwangliang@users.noreply.github.com>
2017-07-16 00:33:58 +0800

    1894功能 V1.2

--- AboutForm.Designer.cs
+++ AboutForm.Designer.cs
@@ -45,7 +45,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(59, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "版本：1.1";
+            this.label1.Text = "版本：1.2";
             // 
             // label2
             // 
